91525ca2c6529559ba1a024ed2b17201009f087a
Jerry Shikongo <shikongo.tj@gmail.com>
2020-05-17 21:10:16 +1200

    Added Guardian

--- medium_graphql_api/config/config.exs
+++ medium_graphql_api/config/config.exs
@@ -25,6 +25,10 @@ config :logger, :console,
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
+# Guardian details
+config :medium_graphql_api, MediumGraphqlApi.Guardian,
+  issuer: "medium_graphql_api",
+  secret_key: "k5nb5f/P+hS7zFlxJTuPBxe5Ecyeos0FCFgDfn4nzbBtHNrewPyyJJjGLoU7Rk/e"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
