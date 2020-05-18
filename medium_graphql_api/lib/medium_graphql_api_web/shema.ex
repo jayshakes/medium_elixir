64eaca15b59d93a63fe9292fd1d717aa8651253c
Jerry Shikongo <shikongo.tj@gmail.com>
2020-05-18 18:36:02 +1200

    Implement athorization middleware, to check roles

--- medium_graphql_api/lib/medium_graphql_api_web/shema.ex
+++ medium_graphql_api/lib/medium_graphql_api_web/shema.ex
@@ -2,12 +2,16 @@ defmodule MediumGraphqlApiWeb.Schema do
   use Absinthe.Schema
 
   alias MediumGraphqlApiWeb.Resolvers
+  alias MediumGraphqlApiWeb.Schema.Middleware
+
   # import Types
   import_types(MediumGraphqlApiWeb.Schema.Types)
 
   query do
     @desc "Get a list of all users"
     field :users, list_of(:user_type) do
+      # Middleware
+      middleware(Middleware.Authorize, "admin")
       # Resolve
       resolve(&Resolvers.UserResolver.users/3)
     end
